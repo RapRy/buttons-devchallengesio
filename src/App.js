ba2a63900416e3847ac0379e2da3be16ffdd40c8
Ryan <ry.quickcell@gmail.com>
2021-11-15 10:52:26 +0800

    final

--- src/App.js
+++ src/App.js
@@ -6,7 +6,40 @@ import Button from "./Button";
 const App = () => {
   return (
     <div>
+      <p>variant default</p>
       <Button />
+      <p>variant outline</p>
+      <Button variant="outline" color="primary" />
+      <p>variant text</p>
+      <Button variant="text" color="primary" />
+      <p>shadow disabled</p>
+      <Button variant="default" color="primary" disableShadow />
+      <p>disabled button</p>
+      <Button variant="default" color="default" disabled />
+      <p>disabled text button</p>
+      <Button variant="text" color="default" disabled />
+      <p>start icon</p>
+      <Button
+        variant="default"
+        color="primary"
+        startIcon="local_grocery_store"
+      />
+      <p>end icon</p>
+      <Button variant="default" color="primary" endIcon="local_grocery_store" />
+      <p>small button</p>
+      <Button variant="default" color="primary" size="sm" />
+      <p>medium button</p>
+      <Button variant="default" color="primary" size="md" />
+      <p>large button</p>
+      <Button variant="default" color="primary" size="lg" />
+      <p>default color</p>
+      <Button color="default" />
+      <p>primary color</p>
+      <Button color="primary" />
+      <p>secondary color</p>
+      <Button color="secondary" />
+      <p>danger color</p>
+      <Button color="danger" />
     </div>
   );
 };
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-const Button = () => {
-  return <button>Default</button>;
+const Button = ({
+  variant,
+  disableShadow,
+  disabled,
+  startIcon,
+  endIcon,
+  size,
+  color,
+}) => {
+  return (
+    <button
+      type="button"
+      data-variant={variant}
+      data-shadow={disableShadow}
+      data-size={size}
+      data-color={color}
+      disabled={disabled === true ? "disabled" : ""}
+    >
+      {startIcon && <span className="material-icons">{startIcon}</span>}
+      Default
+      {endIcon && <span className="material-icons">{endIcon}</span>}
+    </button>
+  );
 };
 
 export default Button;
